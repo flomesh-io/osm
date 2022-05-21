e702628e5dd52768175418c9d89bac170f79ed60
cybwan <baili@flomesh.io>
2022-05-21 20:24:53 +0800

    Switch to flomesh/pipy:latest

--- pipy/scripts/cancel-cache-osm-images.sh
+++ pipy/scripts/cancel-cache-osm-images.sh
@@ -22,6 +22,6 @@ find ${OSM_HOME}/dockerfiles -type f | xargs sed -i 's# localhost:5000/distroles
 find ${OSM_HOME}/dockerfiles -type f | xargs sed -i 's# localhost:5000/flomesh/proxy-wasm-cpp-sdk:v2 AS# flomesh/proxy-wasm-cpp-sdk:v2 AS#g'
 
 sed -i 's#sidecarImage: localhost:5000/envoyproxy/envoy#sidecarImage: envoyproxy/envoy#g' ${OSM_HOME}/charts/osm/values.yaml
-sed -i 's#sidecarImage: localhost:5000/flomesh/pipy-nightly#sidecarImage: flomesh/pipy-nightly#g' ${OSM_HOME}/charts/osm/values.yaml
+sed -i 's#sidecarImage: localhost:5000/flomesh/pipy#sidecarImage: flomesh/pipy#g' ${OSM_HOME}/charts/osm/values.yaml
 sed -i 's#curlImage: localhost:5000/curlimages/curl#curlImage: curlimages/curl#g' ${OSM_HOME}/charts/osm/values.yaml
 sed -i 's#docker.io#localhost:5000#g' ${OSM_HOME}/charts/osm/values.yaml
